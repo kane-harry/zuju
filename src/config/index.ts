a916b8a061ac929cf4029218cc175e86d6edf34c
a <a>
2023-01-09 22:14:48 +0700

    Init code for swagger APIs

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,6 +6,7 @@ dotenv.config()
 export const config = {
     port: Number(process.env.PORT || 3000),
     system: {
-        defaultQueryPageSize: 10
+        defaultQueryPageSize: 10,
+        applicationApiRootURL: String(process.env.APPLICATION_API_ROOT_URL || `http://localhost:${process.env.PORT || 3000}`),
     }
 }
---
+++ src/docs/openApiGenerator.ts
@@ -0,0 +1,21 @@
+import { config } from '@config'
+import data from './apiV1.json'
+
+export const openApiV1Documents = {
+    ...data,
+    openapi: '3.0.3',
+    info: {
+        title: `Zuju`,
+        version: `${process.env.npm_package_version || '1.0'}`,
+        description: `API documentation for Zuju`,
+        contact: {
+            email: 'vuvannhan.hut@gmail.com'
+        }
+    },
+    servers: [
+        {
+            url: `${config.system.applicationApiRootURL}/api/v1`,
+            description: ''
+        }
+    ]
+}
